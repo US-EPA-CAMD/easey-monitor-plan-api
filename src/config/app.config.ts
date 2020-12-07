b99ddebf682f0785e5f0c8cb2083965b4407ab9f
jasonwhitehead <jasonwhitehad@cvpcorp.com>
2020-12-07 13:11:33 -0600

    changed host env var name

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 const title = 'Monitor Plan Management';
 const path = 'api/monitor-plan-mgmt';
-const host = process.env.EASEY_MONITOR_PLAN_MGMT_API_HOST || 'localhost';
+const host = process.env.EASEY_API_HOST || 'localhost';
 const port = process.env.EASEY_MONITOR_PLAN_MGMT_API_PORT || 8000;
 
 let uri = `https://${host}/${path}`
