f76355a8caf5ea064b7ba9485a081fe8758299d3
SimeraHailu <68601178+SimeraHailu@users.noreply.github.com>
2021-03-12 05:32:45 -0600

    components
    components by location

---
+++ src/maps/monitor-system-component.map.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+
+import { BaseMap } from './base.map';
+import { MonitorSystemComponent } from '../entities/monitor-system-component.entity';
+import {MonitorSystemComponentDTO } from '../dtos/monitor-system-component.dto';
+
+
+
+@Injectable()
+export class systemComponentMap extends BaseMap<MonitorSystemComponent, MonitorSystemComponentDTO> {
+  public async one(entity: MonitorSystemComponent): Promise<MonitorSystemComponentDTO> {
+    return {
+        id: entity.id,
+        monSysId: entity.monSysId,    
+        componentId: entity.componentId,
+        beginDate: entity.beginDate,
+        beginHour: entity.beginHour,
+        endDate: entity.endDate,
+        endHour: entity.endHour ,
+    };
+  }
+}
