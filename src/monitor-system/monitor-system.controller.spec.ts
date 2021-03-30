85b27b3709426b97738e0e7c7d96c4141f14d132
SimeraHailu <68601178+SimeraHailu@users.noreply.github.com>
2021-03-30 10:11:28 -0500

    Update monitor-system.controller.spec.ts

--- src/monitor-system/monitor-system.controller.spec.ts
+++ src/monitor-system/monitor-system.controller.spec.ts
@@ -5,13 +5,22 @@ import { MonitorSystemService } from './monitor-system.service';
 import { MonitorSystemRepository } from './monitor-system.repository';
 import { MonitorSystemMap } from '../maps/monitor-system.map';
 import { MonitorSystemDTO } from '../dtos/monitor-system.dto';
+import{ComponentMap} from '../maps/component.map';
+import { MonitorSystemComponent } from '../entities/monitor-system-component.entity';
+import{systemComponentMap} from '../maps/monitor-system-component.map'
+import { MonitorSystemComponentRepository } from './monitor-system-component.repository';
+import { ComponentRepository } from '../component/component.repository';
+
+import { SystemFuelFlowRepository } from './system-fuel-flow.repository';
+import { SystemFuelFlow } from '../entities/system-fuel-flow.entity';
+import { SystemFuelFlowMap } from '../maps/system-fuel-flow.map';
 
 
 const mockConfigService = () => ({
     get: jest.fn(),
   });
   
-  describe('-- Supplemental Methods Controller --', () => {
+  describe('-- Monistor System Controller --', () => {
     let supplementalMethodsController: MonitorSystemController;
     let supplementalMethodsService: MonitorSystemService;
   
@@ -21,7 +30,14 @@ const mockConfigService = () => ({
         providers: [
             MonitorSystemMap,
             MonitorSystemService,
-            MonitorSystemRepository,
+            MonitorSystemRepository,SystemFuelFlow,
+            MonitorSystemComponent,
+            systemComponentMap,
+            ComponentMap,
+            SystemFuelFlowMap,
+            MonitorSystemComponentRepository,
+            ComponentRepository,
+            SystemFuelFlowRepository,
           {
             provide: ConfigService,
             useFactory: mockConfigService,
@@ -41,9 +57,7 @@ const mockConfigService = () => ({
       it('should return a list of Monitoring Systems', async () => {
         const monLocId = '123';
         const expectedResult: MonitorSystemDTO[] = [];
-  
-        
-        expect(2).toBe(2);
+        return true;
       });
     });
 
@@ -51,9 +65,7 @@ const mockConfigService = () => ({
         it('should return a list of Monitoring components', async () => {
           const monLocId = '123';
           const expectedResult: MonitorSystemDTO[] = [];
-    
-    
-          expect(1).toBe(1);
+          return true;
         });
       });
   
@@ -63,11 +75,8 @@ const mockConfigService = () => ({
     it('should return a list of Monitoring fuel flows', async () => {
       const monLocId = '123';
       const expectedResult: MonitorSystemDTO[] = [];
-
-
-      expect(1).toBe(1);
-    });
+      return true;
   });
 }
   );
-  
+});
