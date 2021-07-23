26d69d38a5aaa48d11cc4ea7539b2ed41cd513f4
Jeffrey Aggrey <jeffreyaggrey@gmailcom>
2021-07-23 13:51:35 -0500

    update analyzer range workspace module

--- src/mats-method-workspace/mats-method.module.ts
+++ src/mats-method-workspace/mats-method.module.ts
@@ -4,10 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MatsMethodWorkspaceController } from './mats-method.controller';
 import { MatsMethodWorkspaceService } from './mats-method.service';
 import { MatsMethodWorkspaceRepository } from './mats-method.repository';
+import { MatsMethodMap } from 'src/maps/mats-method.map';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MatsMethodWorkspaceRepository])],
   controllers: [MatsMethodWorkspaceController],
-  providers: [MatsMethodWorkspaceService],
+  providers: [MatsMethodWorkspaceService, MatsMethodMap],
 })
 export class MatsMethodWorkspaceModule {}
