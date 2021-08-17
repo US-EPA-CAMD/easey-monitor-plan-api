5c42e6394a656022894bb29d7613f05b4f472dde
Jeffrey Aggrey <jeffreyaggrey@cvpcorp.com>
2021-08-17 13:16:29 -0500

    add updateAnalyzerRange

--- src/analyzer-range-workspace/analyzer-range.controller.ts
+++ src/analyzer-range-workspace/analyzer-range.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { UpdateAnalyzerRangeDTO } from '../dtos/update-analyzer-range.dto';
 import { AnalyzerRangeDTO } from '../dtos/analyzer-range.dto';
 import { AnalyzerRangeWorkspaceService } from './analyzer-range.service';
 
@@ -19,5 +20,20 @@ export class AnalyzerRangeWorkspacController {
     @Param('compId') compId: string,
   ): Promise<AnalyzerRangeDTO[]> {
     return this.service.getAnalyzerRanges(compId);
+  }
+
+  @Put(':analyzerRangeId')
+  @ApiOkResponse({
+    isArray: false,
+    type: AnalyzerRangeDTO,
+    description: 'Updates workspace Analyzer Range record for a component',
+  })
+  updateAnalyserRange(
+    @Param('locId') locId: string,
+    @Param('compId') compId: string,
+    @Param('analyzerRangeId') analyzerRangeId: string,
+    @Body() payload: UpdateAnalyzerRangeDTO,
+  ) {
+    return this.service.updateAnalyzerRangd(analyzerRangeId, payload);
   }
 }
--- src/analyzer-range-workspace/analyzer-range.service.ts
+++ src/analyzer-range-workspace/analyzer-range.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AnalyzerRangeDTO } from 'src/dtos/analyzer-range.dto';
+import { UpdateAnalyzerRangeDTO } from 'src/dtos/update-analyzer-range.dto';
 import { AnalyzerRangeMap } from 'src/maps/analyzer-range.map';
 
 import { AnalyzerRangeWorkspaceRepository } from './analyzer-range.repository';
@@ -16,5 +17,33 @@ export class AnalyzerRangeWorkspaceService {
   async getAnalyzerRanges(compId: string): Promise<AnalyzerRangeDTO[]> {
     const results = await this.repository.find({ componentId: compId });
     return this.map.many(results);
+  }
+
+  async getAnalyzerRange(analyzerRangeId: string): Promise<AnalyzerRangeDTO> {
+    const result = await this.repository.findOne(analyzerRangeId);
+
+    if (!result) {
+      throw new NotFoundException('Analyzer Range not found.');
+    }
+
+    return this.map.one(result);
+  }
+
+  async updateAnalyzerRangd(
+    analyzerRangeId: string,
+    payload: UpdateAnalyzerRangeDTO,
+  ): Promise<AnalyzerRangeDTO> {
+    const analyzerRange = await this.getAnalyzerRange(analyzerRangeId);
+
+    analyzerRange.analyzerRangeCode = payload.analyzerRangeCode;
+    analyzerRange.dualRangeIndicator = payload.dualRangeIndicator;
+    analyzerRange.beginDate = payload.beginDate;
+    analyzerRange.beginHour = payload.beginHour;
+    analyzerRange.endDate = payload.endDate;
+    analyzerRange.endHour = payload.endHour;
+
+    const result = await this.repository.save(analyzerRange);
+
+    return this.map.one(result);
   }
 }
