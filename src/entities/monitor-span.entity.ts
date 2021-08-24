07e55aabc6e524a25fa9d6394692feea7f0c1e3d
Jeffrey Aggrey <jeffreyaggrey@cvpcorp.com>
2021-08-24 09:48:00 -0500

    update span entity

--- src/entities/monitor-span.entity.ts
+++ src/entities/monitor-span.entity.ts
@@ -17,49 +17,55 @@ export class MonitorSpan extends BaseEntity {
   @Column({ type: 'varchar', length: 45, nullable: false, name: 'mon_loc_id' })
   locationId: string;
 
-  @Column({ name: 'mpc_value' })
+  @Column({ type: 'numeric', precision: 6, scale: 1, name: 'mpc_value' })
   mpcValue: number;
 
-  @Column({ name: 'mec_value' })
+  @Column({ type: 'numeric', precision: 6, scale: 1, name: 'mec_value' })
   mecValue: number;
 
-  @Column({ name: 'mpf_value' })
+  @Column({ type: 'numeric', precision: 10, scale: 0, name: 'mpf_value' })
   mpfValue: number;
 
-  @Column({ name: 'max_low_range' })
+  @Column({ type: 'numeric', precision: 6, scale: 1, name: 'max_low_range' })
   maxLowRange: number;
 
-  @Column({ name: 'span_value' })
+  @Column({ type: 'numeric', precision: 13, scale: 3, name: 'span_value' })
   spanValue: number;
 
-  @Column({ name: 'full_scale_range' })
+  @Column({
+    type: 'numeric',
+    precision: 13,
+    scale: 3,
+    name: 'full_scale_range',
+  })
   fullScaleRange: number;
 
-  @Column({ type: 'date', name: 'begin_date' })
-  beginDate: Date;
+  @Column({ type: 'varchar', length: 7, name: 'span_uom_cd' })
+  spanUnitsOfMeasureCode: string;
 
-  @Column({ name: 'begin_hour' })
-  beginHour: number;
-
-  @Column({ type: 'date', name: 'end_date' })
-  endDate: Date;
-
-  @Column({ name: 'end_hour' })
-  endHour: number;
-
-  @Column({ name: 'default_high_range' })
+  @Column({
+    type: 'numeric',
+    precision: 5,
+    scale: 0,
+    name: 'default_high_range',
+  })
   defaultHighRange: number;
 
-  @Column({ name: 'flow_span_value' })
+  @Column({ type: 'numeric', precision: 10, scale: 0, name: 'flow_span_value' })
   flowSpanValue: number;
 
-  @Column({ name: 'flow_full_scale_range' })
+  @Column({
+    type: 'numeric',
+    precision: 10,
+    scale: 0,
+    name: 'flow_full_scale_range',
+  })
   flowFullScaleRange: number;
 
   @Column({
     type: 'varchar',
     length: 7,
-
+    nullable: false,
     name: 'component_type_cd',
   })
   componentTypeCode: string;
@@ -70,23 +76,40 @@ export class MonitorSpan extends BaseEntity {
   @Column({
     type: 'varchar',
     length: 7,
-
     name: 'span_method_cd',
   })
   spanMethodCode: string;
 
+  @Column({ type: 'date', nullable: false, name: 'begin_date' })
+  beginDate: Date;
+
+  @Column({
+    type: 'numeric',
+    precision: 2,
+    scale: 0,
+    nullable: false,
+    name: 'begin_hour',
+  })
+  beginHour: number;
+
+  @Column({ type: 'date', name: 'end_date' })
+  endDate: Date;
+
+  @Column({ type: 'numeric', precision: 2, scale: 0, name: 'end_hour' })
+  endHour: number;
+
   @Column({ type: 'varchar', length: 8, name: 'userid' })
   userid: string;
+
+  @Column({ type: 'date', name: 'add_date' })
+  addDate: Date;
 
   @Column({ type: 'date', name: 'update_date' })
   updateDate: Date;
 
-  @Column({ type: 'varchar', length: 7, name: 'span_uom_cd' })
-  spanUnitsOfMeasureCode: string;
-
   @ManyToOne(
     () => MonitorLocation,
-    location => location.spans,
+    ml => ml.spans,
   )
   @JoinColumn({ name: 'mon_loc_id' })
   location: MonitorLocation;
--- src/entities/workspace/monitor-span.entity.ts
+++ src/entities/workspace/monitor-span.entity.ts
@@ -17,76 +17,99 @@ export class MonitorSpan extends BaseEntity {
   @Column({ type: 'varchar', length: 45, nullable: false, name: 'mon_loc_id' })
   locationId: string;
 
-  @Column({ nullable: true, name: 'mpc_value' })
+  @Column({ type: 'numeric', precision: 6, scale: 1, name: 'mpc_value' })
   mpcValue: number;
 
-  @Column({ nullable: true, name: 'mec_value' })
+  @Column({ type: 'numeric', precision: 6, scale: 1, name: 'mec_value' })
   mecValue: number;
 
-  @Column({ nullable: true, name: 'mpf_value' })
+  @Column({ type: 'numeric', precision: 10, scale: 0, name: 'mpf_value' })
   mpfValue: number;
 
-  @Column({ nullable: true, name: 'max_low_range' })
+  @Column({ type: 'numeric', precision: 6, scale: 1, name: 'max_low_range' })
   maxLowRange: number;
 
-  @Column({ nullable: true, name: 'span_value' })
+  @Column({ type: 'numeric', precision: 13, scale: 3, name: 'span_value' })
   spanValue: number;
 
-  @Column({ nullable: true, name: 'full_scale_range' })
+  @Column({
+    type: 'numeric',
+    precision: 13,
+    scale: 3,
+    name: 'full_scale_range',
+  })
   fullScaleRange: number;
 
-  @Column({ type: 'date', nullable: true, name: 'begin_date' })
-  beginDate: Date;
+  @Column({ type: 'varchar', length: 7, name: 'span_uom_cd' })
+  spanUnitsOfMeasureCode: string;
 
-  @Column({ nullable: true, name: 'begin_hour' })
-  beginHour: number;
-
-  @Column({ type: 'date', nullable: true, name: 'end_date' })
-  endDate: Date;
-
-  @Column({ nullable: true, name: 'end_hour' })
-  endHour: number;
-
-  @Column({ nullable: true, name: 'default_high_range' })
+  @Column({
+    type: 'numeric',
+    precision: 5,
+    scale: 0,
+    name: 'default_high_range',
+  })
   defaultHighRange: number;
 
-  @Column({ nullable: true, name: 'flow_span_value' })
+  @Column({ type: 'numeric', precision: 10, scale: 0, name: 'flow_span_value' })
   flowSpanValue: number;
 
-  @Column({ nullable: true, name: 'flow_full_scale_range' })
+  @Column({
+    type: 'numeric',
+    precision: 10,
+    scale: 0,
+    name: 'flow_full_scale_range',
+  })
   flowFullScaleRange: number;
 
   @Column({
     type: 'varchar',
     length: 7,
-    nullable: true,
+    nullable: false,
     name: 'component_type_cd',
   })
   componentTypeCode: string;
 
-  @Column({ type: 'varchar', length: 7, nullable: true, name: 'span_scale_cd' })
+  @Column({ type: 'varchar', length: 7, name: 'span_scale_cd' })
   spanScaleCode: string;
 
   @Column({
     type: 'varchar',
     length: 7,
-    nullable: true,
     name: 'span_method_cd',
   })
   spanMethodCode: string;
 
-  @Column({ type: 'varchar', length: 8, nullable: true, name: 'userid' })
+  @Column({ type: 'date', nullable: false, name: 'begin_date' })
+  beginDate: Date;
+
+  @Column({
+    type: 'numeric',
+    precision: 2,
+    scale: 0,
+    nullable: false,
+    name: 'begin_hour',
+  })
+  beginHour: number;
+
+  @Column({ type: 'date', name: 'end_date' })
+  endDate: Date;
+
+  @Column({ type: 'numeric', precision: 2, scale: 0, name: 'end_hour' })
+  endHour: number;
+
+  @Column({ type: 'varchar', length: 8, name: 'userid' })
   userid: string;
 
-  @Column({ type: 'date', nullable: true, name: 'update_date' })
-  updateDate: Date;
+  @Column({ type: 'date', name: 'add_date' })
+  addDate: Date;
 
-  @Column({ type: 'varchar', length: 7, nullable: true, name: 'span_uom_cd' })
-  spanUnitsOfMeasureCode: string;
+  @Column({ type: 'date', name: 'update_date' })
+  updateDate: Date;
 
   @ManyToOne(
     () => MonitorLocation,
-    location => location.spans,
+    ml => ml.spans,
   )
   @JoinColumn({ name: 'mon_loc_id' })
   location: MonitorLocation;
