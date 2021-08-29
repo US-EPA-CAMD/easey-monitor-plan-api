f09e5fa6e28871dba411b4c5b002300d647d64dc
Jason Whitehead <jwhitehead@austin.rr.com>
2021-08-29 00:32:17 -0500

    fixing typo on analyzer range wks module

--- src/analyzer-range-workspace/analyzer-range.module.ts
+++ src/analyzer-range-workspace/analyzer-range.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AnalyzerRangeWorkspacController } from './analyzer-range.controller';
+import { AnalyzerRangeWorkspaceController } from './analyzer-range.controller';
 import { AnalyzerRangeWorkspaceService } from './analyzer-range.service';
 import { AnalyzerRangeWorkspaceRepository } from './analyzer-range.repository';
 import { AnalyzerRangeMap } from '../maps/analyzer-range.map';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AnalyzerRangeWorkspaceRepository])],
-  controllers: [AnalyzerRangeWorkspacController],
+  controllers: [AnalyzerRangeWorkspaceController],
   providers: [AnalyzerRangeWorkspaceService, AnalyzerRangeMap],
   exports: [TypeOrmModule, AnalyzerRangeWorkspaceService, AnalyzerRangeMap],
 })
