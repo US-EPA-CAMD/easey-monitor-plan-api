0f6a1cc7b6af609f881436f3d7ecff10bcc9d1fb
Jeffrey Aggrey <jeffreyaggrey@cvpcorp.com>
2021-09-10 20:56:44 -0500

    fix param, and pass in locationId, sysId in update component handler

--- src/system-component-workspace/system-component.controller.ts
+++ src/system-component-workspace/system-component.controller.ts
@@ -29,12 +29,17 @@ export class SystemComponentWorkspaceController {
     description: 'Updates workspace component records for a monitor system',
   })
   async upDateComponent(
-    @Param('locId') locatonId: string,
+    @Param('locId') locationId: string,
     @Param('sysId') monSysId: string,
     @Param('compId') componentId: string,
     @Body() payload: UpdateSystemComponentDTO,
   ): Promise<SystemComponentDTO> {
-    return this.service.updateComponent(componentId, payload);
+    return this.service.updateComponent(
+      locationId,
+      monSysId,
+      componentId,
+      payload,
+    );
   }
 
   @Post()
