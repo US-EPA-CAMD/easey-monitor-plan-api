8f421512bf89878e96cf76cca99fba39c1c045f0
Dion Olympia <dionolympia@cvpcorp.com>
2021-09-21 13:01:31 -0400

    post route handler in controller and create method for load

--- src/monitor-load-workspace/monitor-load.controller.ts
+++ src/monitor-load-workspace/monitor-load.controller.ts
@@ -1,8 +1,9 @@
 import { ApiTags, ApiOkResponse } from '@nestjs/swagger';
-import { Get, Param, Controller } from '@nestjs/common';
+import { Get, Param, Controller, Post, Body } from '@nestjs/common';
 
 import { MonitorLoadDTO } from '../dtos/monitor-load.dto';
 import { MonitorLoadWorkspaceService } from './monitor-load.service';
+import { UpdateMonitorLoadDTO } from '../dtos/monitor-load-update.dto';
 
 @ApiTags('Loads')
 @Controller()
@@ -17,5 +18,18 @@ export class MonitorLoadWorkspaceController {
   })
   getLoads(@Param('locId') locationId: string): Promise<MonitorLoadDTO[]> {
     return this.service.getLoads(locationId);
+  }
+
+  @Post()
+  @ApiOkResponse({
+    isArray: true,
+    type: MonitorLoadDTO,
+    description: 'Creates a workspace load record for a monitor location',
+  })
+  createLoad(
+    @Param('locId') locationId: string,
+    @Body() payload: UpdateMonitorLoadDTO,
+  ): Promise<MonitorLoadDTO> {
+    return this.service.createLoad(locationId, payload);
   }
 }
--- src/monitor-load-workspace/monitor-load.service.ts
+++ src/monitor-load-workspace/monitor-load.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { v4 as uuid } from 'uuid';
 
+import { UpdateMonitorLoadDTO } from '../dtos/monitor-load-update.dto';
 import { MonitorLoadDTO } from '../dtos/monitor-load.dto';
 import { MonitorLoadMap } from '../maps/monitor-load.map';
 import { MonitorLoadWorkspaceRepository } from './monitor-load.repository';
@@ -16,5 +18,35 @@ export class MonitorLoadWorkspaceService {
   async getLoads(locationId: string): Promise<MonitorLoadDTO[]> {
     const results = await this.repository.find({ locationId });
     return this.map.many(results);
+  }
+
+  async createLoad(
+    locationId: string,
+    payload: UpdateMonitorLoadDTO,
+  ): Promise<MonitorLoadDTO> {
+    const load = this.repository.create({
+      id: uuid(),
+      locationId,
+      loadAnalysisDate: payload.loadAnalysisDate,
+      beginDate: payload.beginDate,
+      beginHour: payload.beginHour,
+      endDate: payload.endDate,
+      endHour: payload.endHour,
+      maximumLoadValue: payload.maximumLoadValue,
+      secondNormalIndicator: payload.secondNormalIndicator,
+      upperOperationBoundary: payload.upperOperationBoundary,
+      lowerOperationBoundary: payload.lowerOperationBoundary,
+      normalLevelCode: payload.normalLevelCode,
+      secondLevelCode: payload.secondLevelCode,
+      maximumLoadUnitsOfMeasureCode: payload.maximumLoadUnitsOfMeasureCode,
+      // TODO
+      userId: 'testuser',
+      addDate: new Date(Date.now()),
+      updateDate: new Date(Date.now()),
+    });
+
+    const result = await this.repository.save(load);
+
+    return this.map.one(result);
   }
 }
