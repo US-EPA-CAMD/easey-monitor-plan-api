b1afb58982e4a16ea69e3ec563e9c2f550476168
Jeffrey Aggrey <jeffreyaggrey@cvpcorp.com>
2021-09-22 17:59:05 -0500

    add PUT and POST mp-formula-data

--- src/monitor-formula-workspace/monitor-formula.repository.ts
+++ src/monitor-formula-workspace/monitor-formula.repository.ts
@@ -5,4 +5,11 @@ import { MonitorFormula } from '../entities/workspace/monitor-formula.entity';
 @EntityRepository(MonitorFormula)
 export class MonitorFormulaWorkspaceRepository extends Repository<
   MonitorFormula
-> {}
+> {
+  async getFormula(locationId: string, id: string): Promise<MonitorFormula> {
+    return this.createQueryBuilder('mf')
+      .where('mf.locationId = :locationId', { locationId })
+      .andWhere('mf.id = :id ', { id })
+      .getOne();
+  }
+}
