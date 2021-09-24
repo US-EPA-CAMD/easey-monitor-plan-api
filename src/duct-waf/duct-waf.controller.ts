fa36456334299da9d3ed5ceeaadbf751729a0f0e
Jason Whitehead <jwhitehead@austin.rr.com>
2021-09-24 15:14:24 -0500

    adding rect duct waf official GET to force dev build

--- src/duct-waf/duct-waf.controller.ts
+++ src/duct-waf/duct-waf.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { ApiTags, ApiOkResponse } from '@nestjs/swagger';
+import { Get, Param, Controller } from '@nestjs/common';
 
 import { DuctWafService } from './duct-waf.service';
+import { DuctWafDTO } from 'src/dtos/duct-waf.dto';
 
+@ApiTags('Rectangular Duct WAF')
 @Controller()
 export class DuctWafController {
   constructor(private readonly service: DuctWafService) {}
+
+  @Get()
+  @ApiOkResponse({
+    isArray: true,
+    type: DuctWafDTO,
+    description: 'Retrieves official duct waf records for a monitor location',
+  })
+  getDuctWafs(@Param('locId') locationId: string): Promise<DuctWafDTO[]> {
+    return this.service.getDuctWafs(locationId);
+  }
 }
