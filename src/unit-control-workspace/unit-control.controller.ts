9049d55b9e4ebfdc7f49970b73dd9eceb7386495
Dion Olympia <dionolympia@cvpcorp.com>
2021-10-15 16:11:35 -0400

    PUT and POST endpoints for unit controls

--- src/unit-control-workspace/unit-control.controller.ts
+++ src/unit-control-workspace/unit-control.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { UpdateUnitControlDTO } from '../dtos/unit-control-update.dto';
 import { UnitControlDTO } from '../dtos/unit-control.dto';
 
 import { UnitControlWorkspaceService } from './unit-control.service';
@@ -18,5 +19,30 @@ export class UnitControlWorkspaceController {
   })
   getUnitControls(@Param('unitId') unitId: number): Promise<UnitControlDTO[]> {
     return this.service.getUnitControls(unitId);
+  }
+  @Put(':unitControlId')
+  @ApiOkResponse({
+    type: UnitControlDTO,
+    description: 'Updates a workspace unit control record by unit control ID',
+  })
+  async updateLoad(
+    @Param('unitId') unitId: number,
+    @Param('unitControlId') unitControlId: string,
+    @Body() payload: UpdateUnitControlDTO,
+  ): Promise<UnitControlDTO> {
+    return this.service.updateUnitControl(unitControlId, unitId, payload);
+  }
+
+  @Post()
+  @ApiOkResponse({
+    isArray: true,
+    type: UnitControlDTO,
+    description: 'Creates a workspace unit control record for a unit',
+  })
+  createLoad(
+    @Param('unitId') unitId: number,
+    @Body() payload: UpdateUnitControlDTO,
+  ): Promise<UnitControlDTO> {
+    return this.service.createUnitControl(unitId, payload);
   }
 }
--- src/unit-control-workspace/unit-control.service.ts
+++ src/unit-control-workspace/unit-control.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UpdateUnitControlDTO } from 'src/dtos/unit-control-update.dto';
 import { UnitControlDTO } from '../dtos/unit-control.dto';
 import { UnitControlMap } from '../maps/unit-control.map';
+import { v4 as uuid } from 'uuid';
 
 import { UnitControlWorkspaceRepository } from './unit-control.repository';
 
@@ -16,5 +18,57 @@ export class UnitControlWorkspaceService {
   async getUnitControls(unitId: number): Promise<UnitControlDTO[]> {
     const results = await this.repository.find({ unitId });
     return this.map.many(results);
+  }
+
+  async getUnitControl(id: string): Promise<UnitControlDTO> {
+    const result = await this.repository.findOne(id);
+    if (!result) {
+      throw new NotFoundException('Monitor Load not found');
+    }
+    return this.map.one(result);
+  }
+
+  async createUnitControl(
+    unitId: number,
+    payload: UpdateUnitControlDTO,
+  ): Promise<UnitControlDTO> {
+    const load = this.repository.create({
+      id: uuid(),
+      unitId,
+      controlCode: payload.controlCode,
+      parameterCode: payload.parameterCode,
+      installDate: payload.installDate,
+      optimizationDate: payload.optimizationDate,
+      originalCode: payload.originalCode,
+      retireDate: payload.retireDate,
+      seasonalControlsIndicator: payload.seasonalControlsIndicator,
+      userId: 'testuser',
+      addDate: new Date(Date.now()),
+      updateDate: new Date(Date.now()),
+    });
+
+    const result = await this.repository.save(load);
+    return this.map.one(result);
+  }
+
+  async updateUnitControl(
+    unitControlId: string,
+    unitId: number,
+    payload: UpdateUnitControlDTO,
+  ): Promise<UnitControlDTO> {
+    const unitControl = await this.getUnitControl(unitControlId);
+
+    unitControl.controlCode = payload.controlCode;
+    unitControl.parameterCode = payload.parameterCode;
+    unitControl.installDate = payload.installDate;
+    unitControl.optimizationDate = payload.optimizationDate;
+    unitControl.originalCode = payload.originalCode;
+    unitControl.retireDate = payload.retireDate;
+    unitControl.seasonalControlsIndicator = payload.seasonalControlsIndicator;
+    unitControl.userId = 'testuser';
+    unitControl.updateDate = new Date(Date.now());
+
+    await this.repository.save(unitControl);
+    return this.getUnitControl(unitControlId);
   }
 }
