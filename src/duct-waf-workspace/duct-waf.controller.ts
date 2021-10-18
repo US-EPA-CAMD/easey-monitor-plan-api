b3396246ef1a8ab6234f08042861959d4f391b45
jeffreyaggrey <85371801+jeffreyaggrey@users.noreply.github.com>
2021-10-18 10:56:49 -0500

    Feature/1709/edit rectangular duct wafs (#106)
    * view workspace rectangular duct wafs data
    
    * edit rectangular-duct-waf

--- src/duct-waf-workspace/duct-waf.controller.ts
+++ src/duct-waf-workspace/duct-waf.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Put, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+import { AuthGuard } from '../guards/auth.guard';
+import { UpdateDuctWafDTO } from '../dtos/duct-waf-update.dto';
 import { DuctWafDTO } from '../dtos/duct-waf.dto';
 import { DuctWafWorkspaceService } from './duct-waf.service';
+import CurrentUser from 'src/decorators/current-user.decorator';
 
 @ApiTags('Rectangular Duct WAF')
 @Controller()
@@ -13,9 +16,30 @@ export class DuctWafWorkspaceController {
   @ApiOkResponse({
     isArray: true,
     type: DuctWafDTO,
-    description: 'Retrieves official duct waf records for a monitor location',
+    description: 'Retrieves workspace duct waf records for a monitor location',
   })
   getDuctWafs(@Param('locId') locationId: string): Promise<DuctWafDTO[]> {
     return this.service.getDuctWafs(locationId);
+  }
+
+  @Put(':ductWafId')
+  @ApiBearerAuth('Token')
+  @UseGuards(AuthGuard)
+  @ApiOkResponse({
+    type: DuctWafDTO,
+    description: 'Updates a workspace duct waf record for a monitor location',
+  })
+  async updateDuctWat(
+    @Param('locId') locationId: string,
+    @Param('ductWafId') ductWafId: string,
+    @Body() payload: UpdateDuctWafDTO,
+    @CurrentUser() userId: string,
+  ): Promise<DuctWafDTO> {
+    return await this.service.updateDuctWaf(
+      locationId,
+      ductWafId,
+      payload,
+      userId,
+    );
   }
 }
