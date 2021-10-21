9884c3366c6b7b07beb49276f7bdd5ef8c53ba21
Kyle Herceg <kdherceg@email.wm.edu>
2021-10-21 11:57:44 -0400

    Fixed typeorm issue and added helmet

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { CorsOptionsService } from '@us-epa-camd/easey-common/cors-options';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -29,6 +30,7 @@ async function bootstrap() {
     };
   }
 
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix(appPath);
   app.enableCors(async (req, callback) => {
