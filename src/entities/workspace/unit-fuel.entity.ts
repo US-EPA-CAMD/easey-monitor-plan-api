317686773d4087cee9a2775fccfde21a4701d2b1
Dion Olympia <dionolympia@cvpcorp.com>
2021-10-22 15:13:21 -0400

    fixing date fields in entity files for unit controls and fuels

--- src/entities/workspace/unit-fuel.entity.ts
+++ src/entities/workspace/unit-fuel.entity.ts
@@ -19,10 +19,10 @@ export class UnitFuel extends BaseEntity {
   @Column({ name: 'fuel_type' })
   fuelCode: string;
 
-  @Column({ name: 'begin_date' })
+  @Column({ type: 'date', name: 'begin_date' })
   beginDate: Date;
 
-  @Column({ name: 'end_date' })
+  @Column({ type: 'date', name: 'end_date' })
   endDate: Date;
 
   @Column({ name: 'indicator_cd' })
@@ -46,10 +46,10 @@ export class UnitFuel extends BaseEntity {
   @Column({ name: 'userid' })
   userId: string;
 
-  @Column({ name: 'add_date' })
+  @Column({ type: 'date', name: 'add_date' })
   addDate: Date;
 
-  @Column({ name: 'update_date' })
+  @Column({ type: 'date', name: 'update_date' })
   updateDate: Date;
 
   @ManyToOne(
