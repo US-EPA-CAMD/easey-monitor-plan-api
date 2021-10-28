36808090cca8555a8aeda5278e3ecf2c363815fa
dionolympiaCVP <90858230+dionolympiaCVP@users.noreply.github.com>
2021-10-28 13:58:27 -0400

    Feature/2191 endpoints for qual percent (#120)
    * added POST & PUT endpoints and unit testing
    
    * adding ordering by percent qual id

---
+++ src/pct-qualification-workspace/pct-qualification.repository.spec.ts
@@ -0,0 +1,79 @@
+import { Test } from '@nestjs/testing';
+import { SelectQueryBuilder } from 'typeorm';
+
+import { PCTQualificationWorkspaceRepository } from './pct-qualification.repository';
+import { PCTQualification } from '../entities/workspace/pct-qualification.entity';
+
+const mockQueryBuilder = () => ({
+  innerJoinAndSelect: jest.fn(),
+  leftJoinAndSelect: jest.fn(),
+  where: jest.fn(),
+  andWhere: jest.fn(),
+  addOrderBy: jest.fn(),
+  getMany: jest.fn(),
+  getOne: jest.fn(),
+});
+
+describe('PCTQualificationWorkspaceRepository', () => {
+  let pctQualificationRepository;
+  let queryBuilder;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        PCTQualificationWorkspaceRepository,
+        { provide: SelectQueryBuilder, useFactory: mockQueryBuilder },
+      ],
+    }).compile();
+
+    pctQualificationRepository = module.get(
+      PCTQualificationWorkspaceRepository,
+    );
+    queryBuilder = module.get<SelectQueryBuilder<PCTQualification>>(
+      SelectQueryBuilder,
+    );
+  });
+
+  describe('getPCTQualification', () => {
+    it('calls createQueryBuilder and gets all PCTQualifications from the repository with the specified facId', async () => {
+      pctQualificationRepository.createQueryBuilder = jest
+        .fn()
+        .mockReturnValue(queryBuilder);
+      queryBuilder.innerJoinAndSelect.mockReturnValue(queryBuilder);
+      queryBuilder.where.mockReturnValue(queryBuilder);
+      queryBuilder.andWhere.mockReturnValue(queryBuilder);
+      queryBuilder.addOrderBy.mockReturnValue(queryBuilder);
+      queryBuilder.getOne.mockReturnValue('mockPCTQualification');
+
+      const result = await pctQualificationRepository.getPCTQualification(
+        0,
+        0,
+        0,
+      );
+
+      expect(queryBuilder.getOne).toHaveBeenCalled();
+      expect(result).toEqual('mockPCTQualification');
+    });
+  });
+
+  describe('getPCTQualifications', () => {
+    it('calls createQueryBuilder and gets all PCTQualifications from the repository with the specified facId', async () => {
+      pctQualificationRepository.createQueryBuilder = jest
+        .fn()
+        .mockReturnValue(queryBuilder);
+      queryBuilder.innerJoinAndSelect.mockReturnValue(queryBuilder);
+      queryBuilder.where.mockReturnValue(queryBuilder);
+      queryBuilder.andWhere.mockReturnValue(queryBuilder);
+      queryBuilder.addOrderBy.mockReturnValue(queryBuilder);
+      queryBuilder.getMany.mockReturnValue('mockPCTQualifications');
+
+      const result = await pctQualificationRepository.getPCTQualifications(
+        0,
+        0,
+      );
+
+      expect(queryBuilder.getMany).toHaveBeenCalled();
+      expect(result).toEqual('mockPCTQualifications');
+    });
+  });
+});
