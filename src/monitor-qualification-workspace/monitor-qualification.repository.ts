841ba8b0e70c93a8bbc6624eae3aafabc09ed79b
Dion Olympia <dionolympia@cvpcorp.com>
2021-10-28 18:42:35 -0400

    PUT and POST endpoints for monitor qualifications

--- src/monitor-qualification-workspace/monitor-qualification.repository.ts
+++ src/monitor-qualification-workspace/monitor-qualification.repository.ts
@@ -5,4 +5,16 @@ import { MonitorQualification } from '../entities/workspace/monitor-qualificatio
 @EntityRepository(MonitorQualification)
 export class MonitorQualificationWorkspaceRepository extends Repository<
   MonitorQualification
-> {}
+> {
+  async getQualification(
+    locId: string,
+    qualId: string,
+  ): Promise<MonitorQualification> {
+    return this.createQueryBuilder('q')
+      .innerJoinAndSelect('q.location', 'l')
+      .where('l.id = :locId', { locId })
+      .andWhere('q.id = :qualId', { qualId })
+      .addOrderBy('q.id')
+      .getOne();
+  }
+}
