07b3a34a2d0cf11cc49d3333d927fb85396d5fda
Dion Olympia <dionolympia@cvpcorp.com>
2022-01-31 14:53:06 -0500

    unit test for monitor-plan repository (global view)

---
+++ src/monitor-plan/monitor-plan.repository.spec.ts
@@ -0,0 +1,66 @@
+import { Test } from '@nestjs/testing';
+import { SelectQueryBuilder } from 'typeorm';
+
+import { MonitorPlanRepository } from './monitor-plan.repository';
+import { MonitorPlan } from '../entities/monitor-plan.entity';
+
+const mp = new MonitorPlan();
+const mpArray = [];
+mpArray.push(mp);
+
+const mockQueryBuilder = () => ({
+  getOne: jest.fn(),
+  getMany: jest.fn(),
+  where: jest.fn(),
+  innerJoin: jest.fn(),
+  innerJoinAndSelect: jest.fn(() => mpArray),
+});
+
+describe('-- Monitor Plan Repository --', () => {
+  let monitorPlanRepository;
+  let queryBuilder;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        MonitorPlanRepository,
+        { provide: SelectQueryBuilder, useFactory: mockQueryBuilder },
+      ],
+    }).compile();
+
+    monitorPlanRepository = module.get(MonitorPlanRepository);
+    queryBuilder = module.get<SelectQueryBuilder<MonitorPlan>>(
+      SelectQueryBuilder,
+    );
+  });
+
+  it('calls createQueryBuilder and gets all MonitorPlan data from the repository', async () => {
+    monitorPlanRepository.createQueryBuilder = jest
+      .fn()
+      .mockReturnValue(queryBuilder);
+    queryBuilder.innerJoin.mockReturnValue(queryBuilder);
+    queryBuilder.innerJoinAndSelect.mockReturnValue(queryBuilder);
+    queryBuilder.getMany.mockReturnValue(mpArray);
+
+    const orisCode = 123;
+    const result = await monitorPlanRepository.getMonitorPlansByOrisCode(
+      orisCode,
+    );
+    expect(queryBuilder.getMany).toHaveBeenCalled();
+    expect(result).toEqual(mpArray);
+  });
+
+  it('calls createQueryBuilder and gets data for a specific MonitorPlan from the repository', async () => {
+    monitorPlanRepository.createQueryBuilder = jest
+      .fn()
+      .mockReturnValue(queryBuilder);
+    queryBuilder.innerJoin.mockReturnValue(queryBuilder);
+    queryBuilder.innerJoinAndSelect.mockReturnValue(queryBuilder);
+    queryBuilder.where.mockReturnValue(queryBuilder);
+    queryBuilder.getOne.mockReturnValue(mp);
+
+    const result = await monitorPlanRepository.getMonitorPlan(1);
+    expect(queryBuilder.getOne).toHaveBeenCalled();
+    expect(result).toEqual({});
+  });
+});
