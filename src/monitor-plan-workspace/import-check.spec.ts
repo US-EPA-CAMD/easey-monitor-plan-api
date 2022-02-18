e8af1a9c4c398c0f9f6e3bcae022ec1f81bbee2a
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-02-18 14:37:18 -0500

    Fixed broken import test cases

--- src/monitor-plan-workspace/import-check.spec.ts
+++ src/monitor-plan-workspace/import-check.spec.ts
@@ -29,158 +29,8 @@ describe('Monitor-Import Utilities Tests', () => {
 });
 
 describe('Monitor-Import File Check Tests', () => {
-  describe('Check3', () => {
-    it('Should error with two error messages given invalid unitId and stackPipeId', () => {
-      const testData = new UpdateMonitorPlanDTO();
-      testData.unitStackConfiguration = [
-        {
-          unitId: null,
-          stackPipeId: null,
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-      ];
-
-      const checkResults = checks.Check3(testData);
-
-      expect(checkResults.checkResult).toBe(false);
-      expect(checkResults.checkErrorMessages.length).toBe(2);
-    });
-
-    it('Should error with one error messages given duplicate unitId and stackPipeId', () => {
-      const testData = new UpdateMonitorPlanDTO();
-      testData.unitStackConfiguration = [
-        {
-          unitId: 1,
-          stackPipeId: 'test',
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-        {
-          unitId: 1,
-          stackPipeId: 'test',
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-      ];
-
-      const checkResults = checks.Check3(testData);
-
-      expect(checkResults.checkResult).toBe(false);
-      expect(checkResults.checkErrorMessages.length).toBe(1);
-    });
-
-    it('Should pass with valid input', () => {
-      const testData = new UpdateMonitorPlanDTO();
-      testData.unitStackConfiguration = [
-        {
-          unitId: 1,
-          stackPipeId: 'test',
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-      ];
-
-      const checkResults = checks.Check3(testData);
-
-      expect(checkResults.checkResult).toBe(true);
-    });
-  });
-
-  describe('Check4', () => {
-    it('Should error with no matching stackPipeID in location and configuration data', () => {
-      const testData = new UpdateMonitorPlanDTO();
-      testData.unitStackConfiguration = [
-        {
-          unitId: null,
-          stackPipeId: 'test',
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-      ];
-
-      const locationData = new UpdateMonitorLocationDTO();
-      locationData.stackPipeId = 'testFail';
-
-      testData.locations = [locationData];
-
-      const checkResults = checks.Check4(testData);
-
-      expect(checkResults.checkResult).toBe(false);
-      expect(checkResults.checkErrorMessages.length).toBe(1);
-    });
-
-    it('Should pass with matching stackPipeID in location and configuration data', () => {
-      const testData = new UpdateMonitorPlanDTO();
-      testData.unitStackConfiguration = [
-        {
-          unitId: null,
-          stackPipeId: 'test',
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-      ];
-
-      const locationData = new UpdateMonitorLocationDTO();
-      locationData.stackPipeId = 'test';
-
-      testData.locations = [locationData];
-
-      const checkResults = checks.Check4(testData);
-
-      expect(checkResults.checkResult).toBe(true);
-    });
-  });
-
-  describe('Check8', () => {
-    it('Should error with no matching unitId in location and configuration data', () => {
-      const testData = new UpdateMonitorPlanDTO();
-      testData.unitStackConfiguration = [
-        {
-          unitId: 1,
-          stackPipeId: 'test',
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-      ];
-
-      const locationData = new UpdateMonitorLocationDTO();
-      locationData.unitId = '2';
-
-      testData.locations = [locationData];
-
-      const checkResults = checks.Check8(testData);
-
-      expect(checkResults.checkResult).toBe(false);
-      expect(checkResults.checkErrorMessages.length).toBe(1);
-    });
-
-    it('Should pass with matching unitId in location and configuration data', () => {
-      const testData = new UpdateMonitorPlanDTO();
-      testData.unitStackConfiguration = [
-        {
-          unitId: 1,
-          stackPipeId: 'test',
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-      ];
-
-      const locationData = new UpdateMonitorLocationDTO();
-      locationData.unitId = '1';
-
-      testData.locations = [locationData];
-
-      const checkResults = checks.Check8(testData);
-
-      expect(checkResults.checkResult).toBe(true);
-    });
-  });
-});
-
-describe('Monitor-Import Database Check Tests', () => {
   describe('Check1', () => {
-    it('Should pass with corresponding facility id', async () => {
+    it('Should error with no corresponding facility id', async () => {
       const testData = new UpdateMonitorPlanDTO();
       jest.spyOn(checks, 'getFacIdFromOris').mockResolvedValue(null);
 
@@ -190,28 +40,23 @@ describe('Monitor-Import Database Check Tests', () => {
     });
   });
 
-  describe('Check2A', () => {
+  describe('Check2', () => {
     it('Should pass given a returned entry for faciliyId and unitId in uniStackConfiguration', async () => {
       const mockManager = {
         findOne: jest.fn().mockResolvedValue({}),
       };
       jest.spyOn(checks, 'getEntityManager').mockReturnValue(mockManager);
       const testData = new UpdateMonitorPlanDTO();
-      testData.unitStackConfiguration = [
-        {
-          unitId: 1,
-          stackPipeId: 'test',
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-      ];
+
+      const locationData = new UpdateMonitorLocationDTO();
+      locationData.stackPipeId = 'test';
+      locationData.unitId = '1';
+
+      testData.locations = [locationData];
 
       jest.spyOn(checks, 'getFacIdFromOris').mockResolvedValue(1);
 
-      const checkResults = await checks.Check2A(
-        testData,
-        'unitStackConfiguration',
-      );
+      const checkResults = await checks.Check2(testData);
 
       expect(checkResults.checkResult).toBe(true);
     });
@@ -222,73 +67,21 @@ describe('Monitor-Import Database Check Tests', () => {
       };
       jest.spyOn(checks, 'getEntityManager').mockReturnValue(mockManager);
       const testData = new UpdateMonitorPlanDTO();
-      testData.unitStackConfiguration = [
-        {
-          unitId: 1,
-          stackPipeId: 'test',
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-      ];
+      const locationData = new UpdateMonitorLocationDTO();
+      locationData.stackPipeId = 'test';
+      locationData.unitId = '1';
+
+      testData.locations = [locationData];
 
       jest.spyOn(checks, 'getFacIdFromOris').mockResolvedValue(1);
 
-      const checkResults = await checks.Check2A(
-        testData,
-        'unitStackConfiguration',
-      );
+      const checkResults = await checks.Check2(testData);
 
       expect(checkResults.checkResult).toBe(false);
     });
   });
 
-  describe('Check2B', () => {
-    it('Should pass with valid unitId and facilityId', async () => {
-      const mockManager = {
-        findOne: jest.fn().mockResolvedValue({}),
-      };
-      jest.spyOn(checks, 'getEntityManager').mockReturnValue(mockManager);
-      const testData = new UpdateMonitorPlanDTO();
-      jest.spyOn(checks, 'getFacIdFromOris').mockResolvedValue(1);
-
-      testData.unitStackConfiguration = [
-        {
-          unitId: 1,
-          stackPipeId: 'test',
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-      ];
-
-      const checkResults = await checks.Check2B(testData);
-
-      expect(checkResults.checkResult).toBe(true);
-    });
-
-    it('Should error with invalid unitId and facilityId', async () => {
-      const mockManager = {
-        findOne: jest.fn().mockResolvedValue(undefined),
-      };
-      jest.spyOn(checks, 'getEntityManager').mockReturnValue(mockManager);
-      const testData = new UpdateMonitorPlanDTO();
-      jest.spyOn(checks, 'getFacIdFromOris').mockResolvedValue(1);
-
-      testData.unitStackConfiguration = [
-        {
-          unitId: 1,
-          stackPipeId: 'test',
-          beginDate: new Date(),
-          endDate: new Date(),
-        },
-      ];
-
-      const checkResults = await checks.Check2B(testData);
-
-      expect(checkResults.checkResult).toBe(false);
-    });
-  });
-
-  describe('Check2C', () => {
+  describe('Check3', () => {
     it('Should pass with valid unitId and stackId', async () => {
       const mockManager = {
         findOne: jest.fn().mockResolvedValue({}),
@@ -306,7 +99,7 @@ describe('Monitor-Import Database Check Tests', () => {
         },
       ];
 
-      const checkResults = await checks.Check2C(testData);
+      const checkResults = await checks.Check3(testData);
 
       expect(checkResults.checkResult).toBe(true);
     });
@@ -328,7 +121,158 @@ describe('Monitor-Import Database Check Tests', () => {
         },
       ];
 
-      const checkResults = await checks.Check2C(testData);
+      const checkResults = await checks.Check3(testData);
+
+      expect(checkResults.checkResult).toBe(false);
+    });
+  });
+
+  describe('Check4', () => {
+    it('Should pass with valid unitId and facilityId', async () => {
+      const mockManager = {
+        findOne: jest.fn().mockResolvedValue({}),
+      };
+      jest.spyOn(checks, 'getEntityManager').mockReturnValue(mockManager);
+      const testData = new UpdateMonitorPlanDTO();
+      jest.spyOn(checks, 'getFacIdFromOris').mockResolvedValue(1);
+
+      testData.unitStackConfiguration = [
+        {
+          unitId: 1,
+          stackPipeId: 'test',
+          beginDate: new Date(),
+          endDate: new Date(),
+        },
+      ];
+
+      const checkResults = await checks.Check4(testData);
+
+      expect(checkResults.checkResult).toBe(true);
+    });
+
+    it('Should error with invalid unitId and facilityId', async () => {
+      const mockManager = {
+        findOne: jest.fn().mockResolvedValue(undefined),
+      };
+      jest.spyOn(checks, 'getEntityManager').mockReturnValue(mockManager);
+      const testData = new UpdateMonitorPlanDTO();
+      jest.spyOn(checks, 'getFacIdFromOris').mockResolvedValue(1);
+
+      testData.unitStackConfiguration = [
+        {
+          unitId: 1,
+          stackPipeId: 'test',
+          beginDate: new Date(),
+          endDate: new Date(),
+        },
+      ];
+
+      const checkResults = await checks.Check4(testData);
+
+      expect(checkResults.checkResult).toBe(false);
+    });
+  });
+
+  describe('Check8', () => {
+    it('Should pass with valid input', () => {
+      const testData = new UpdateMonitorPlanDTO();
+      testData.unitStackConfiguration = [
+        {
+          unitId: 1,
+          stackPipeId: 'test',
+          beginDate: new Date(),
+          endDate: new Date(),
+        },
+      ];
+
+      const locationData = new UpdateMonitorLocationDTO();
+      locationData.stackPipeId = 'test';
+      locationData.unitId = '1';
+
+      testData.locations = [locationData];
+
+      const checkResults = checks.Check8(testData);
+
+      expect(checkResults.checkResult).toBe(true);
+    });
+
+    it('Should error with one given duplicate unitId and stackPipeId', () => {
+      const testData = new UpdateMonitorPlanDTO();
+      testData.unitStackConfiguration = [
+        {
+          unitId: 1,
+          stackPipeId: 'test',
+          beginDate: new Date(),
+          endDate: new Date(),
+        },
+        {
+          unitId: 1,
+          stackPipeId: 'test',
+          beginDate: new Date(),
+          endDate: new Date(),
+        },
+      ];
+
+      const locationData = new UpdateMonitorLocationDTO();
+      locationData.stackPipeId = 'test';
+      locationData.unitId = '1';
+      testData.locations = [locationData];
+
+      const checkResults = checks.Check8(testData);
+
+      expect(checkResults.checkResult).toBe(false);
+    });
+
+    it('Should error with no matching stackPipeID in location and configuration data', () => {
+      const testData = new UpdateMonitorPlanDTO();
+      testData.unitStackConfiguration = [
+        {
+          unitId: 1,
+          stackPipeId: 'test',
+          beginDate: new Date(),
+          endDate: new Date(),
+        },
+        {
+          unitId: 1,
+          stackPipeId: 'test',
+          beginDate: new Date(),
+          endDate: new Date(),
+        },
+      ];
+
+      const locationData = new UpdateMonitorLocationDTO();
+      locationData.stackPipeId = 'testError';
+      locationData.unitId = '1';
+      testData.locations = [locationData];
+
+      const checkResults = checks.Check8(testData);
+
+      expect(checkResults.checkResult).toBe(false);
+    });
+
+    it('Should error with no matching unitId in location and configuration data', () => {
+      const testData = new UpdateMonitorPlanDTO();
+      testData.unitStackConfiguration = [
+        {
+          unitId: 1,
+          stackPipeId: 'test',
+          beginDate: new Date(),
+          endDate: new Date(),
+        },
+        {
+          unitId: 1,
+          stackPipeId: 'test',
+          beginDate: new Date(),
+          endDate: new Date(),
+        },
+      ];
+
+      const locationData = new UpdateMonitorLocationDTO();
+      locationData.stackPipeId = 'test';
+      locationData.unitId = '2';
+      testData.locations = [locationData];
+
+      const checkResults = checks.Check8(testData);
 
       expect(checkResults.checkResult).toBe(false);
     });
