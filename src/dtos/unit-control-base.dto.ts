0d2e6814d3b2f5f2ba4f8219e15c10e7a715e651
dionolympiaCVP <90858230+dionolympiaCVP@users.noreply.github.com>
2022-03-07 09:52:32 -0500

    fixed bug where service was mapping dto instead of entity (#189)
    * fixed bug where service was mapping dto instead of entity
    
    * added validation (0/1) for radio buttons (loads, unit fuels, location atts, analyzer ranges, sys comps)"

--- src/dtos/unit-control-base.dto.ts
+++ src/dtos/unit-control-base.dto.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { propertyMetadata } from '@us-epa-camd/easey-common/constants';
+import { IsInt, ValidationArguments } from 'class-validator';
+import { IsInRange } from '@us-epa-camd/easey-common/pipes';
 
 export class UnitControlBaseDTO {
   @ApiProperty({
@@ -20,6 +22,12 @@ export class UnitControlBaseDTO {
     description: propertyMetadata.unitControlDTOOriginalCode.description,
     example: propertyMetadata.unitControlDTOOriginalCode.example,
     name: propertyMetadata.unitControlDTOOriginalCode.fieldLabels.value,
+  })
+  @IsInt()
+  @IsInRange(0, 1, {
+    message: (args: ValidationArguments) => {
+      return `${args.property} [COMPONENT-FATAL-A] The value : ${args.value} for ${args.property} must be within the range of 0 and 1`;
+    },
   })
   originalCode: string;
 
@@ -44,6 +52,12 @@ export class UnitControlBaseDTO {
     name:
       propertyMetadata.unitControlDTOSeasonalControlsIndicator.fieldLabels
         .value,
+  })
+  @IsInt()
+  @IsInRange(0, 1, {
+    message: (args: ValidationArguments) => {
+      return `${args.property} [COMPONENT-FATAL-A] The value : ${args.value} for ${args.property} must be within the range of 0 and 1`;
+    },
   })
   seasonalControlsIndicator: string;
 
--- src/dtos/unit-fuel-base.dto.ts
+++ src/dtos/unit-fuel-base.dto.ts
@@ -1,53 +1,61 @@
-import { ApiProperty } from '@nestjs/swagger'
+import { ApiProperty } from '@nestjs/swagger';
 import { propertyMetadata } from '@us-epa-camd/easey-common/constants';
+import { IsInt, ValidationArguments } from 'class-validator';
+import { IsInRange } from '@us-epa-camd/easey-common/pipes';
 
 export class UnitFuelBaseDTO {
   @ApiProperty({
     description: propertyMetadata.unitFuelDTOFuelCode.description,
     example: propertyMetadata.unitFuelDTOFuelCode.example,
-    name: propertyMetadata.unitFuelDTOFuelCode.fieldLabels.value
+    name: propertyMetadata.unitFuelDTOFuelCode.fieldLabels.value,
   })
   fuelCode: string;
 
   @ApiProperty({
     description: propertyMetadata.unitFuelDTOIndicatorCode.description,
     example: propertyMetadata.unitFuelDTOIndicatorCode.example,
-    name: propertyMetadata.unitFuelDTOIndicatorCode.fieldLabels.value
+    name: propertyMetadata.unitFuelDTOIndicatorCode.fieldLabels.value,
   })
   indicatorCode: string;
 
   @ApiProperty({
     description: propertyMetadata.unitFuelDTOOzoneSeasonIndicator.description,
     example: propertyMetadata.unitFuelDTOOzoneSeasonIndicator.example,
-    name: propertyMetadata.unitFuelDTOOzoneSeasonIndicator.fieldLabels.value
+    name: propertyMetadata.unitFuelDTOOzoneSeasonIndicator.fieldLabels.value,
+  })
+  @IsInt()
+  @IsInRange(0, 1, {
+    message: (args: ValidationArguments) => {
+      return `${args.property} [COMPONENT-FATAL-A] The value : ${args.value} for ${args.property} must be within the range of 0 and 1`;
+    },
   })
   ozoneSeasonIndicator: number;
 
   @ApiProperty({
     description: propertyMetadata.unitFuelDTODemGCV.description,
     example: propertyMetadata.unitFuelDTODemGCV.example,
-    name: propertyMetadata.unitFuelDTODemGCV.fieldLabels.value
+    name: propertyMetadata.unitFuelDTODemGCV.fieldLabels.value,
   })
   demGCV: string;
 
   @ApiProperty({
     description: propertyMetadata.unitFuelDTODemSO2.description,
     example: propertyMetadata.unitFuelDTODemSO2.example,
-    name: propertyMetadata.unitFuelDTODemSO2.fieldLabels.value
+    name: propertyMetadata.unitFuelDTODemSO2.fieldLabels.value,
   })
   demSO2: string;
 
   @ApiProperty({
     description: propertyMetadata.unitFuelDTOBeginDate.description,
     example: propertyMetadata.unitFuelDTOBeginDate.example,
-    name: propertyMetadata.unitFuelDTOBeginDate.fieldLabels.value
+    name: propertyMetadata.unitFuelDTOBeginDate.fieldLabels.value,
   })
   beginDate: Date;
 
   @ApiProperty({
     description: propertyMetadata.unitFuelDTOEndDate.description,
     example: propertyMetadata.unitFuelDTOEndDate.example,
-    name: propertyMetadata.unitFuelDTOEndDate.fieldLabels.value
+    name: propertyMetadata.unitFuelDTOEndDate.fieldLabels.value,
   })
   endDate: Date;
 }
