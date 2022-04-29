7dd22fb8faf400e5cd0ef3d5c0f2bbf7641bae78
subrataroy1 <subrataroy@cvpcorp.com>
2022-04-29 15:38:46 -0400

    refactor: update comment

--- src/entities/workspace/stack-pipe.entity.ts
+++ src/entities/workspace/stack-pipe.entity.ts
@@ -43,11 +43,6 @@ export class StackPipe extends BaseEntity {
   })
   facId: number;
 
-  @Column({
-    name: 'userid',
-  })
-  userId: string;
-
   @ManyToOne(
     () => Plant,
     plant => plant.stackPipes,
--- src/monitor-plan-comment/monitor-plan-comment.service.ts
+++ src/monitor-plan-comment/monitor-plan-comment.service.ts
@@ -1,12 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { UpdateMonitorPlanDTO } from 'src/dtos/monitor-plan-update.dto';
-import { MonitorPlanDTO } from 'src/dtos/monitor-plan.dto';
-import { MonitorPlan } from 'src/entities/workspace/monitor-plan.entity';
-import { getFacIdFromOris } from 'src/import-checks/utilities/utils';
-import { getManager } from 'typeorm';
 
-import { MonitorPlanCommentBaseDTO, MonitorPlanCommentDTO } from '../dtos/monitor-plan-comment.dto';
+import { MonitorPlanCommentDTO } from '../dtos/monitor-plan-comment.dto';
 import { MonitorPlanCommentMap } from '../maps/monitor-plan-comment.map';
 import { MonitorPlanCommentRepository } from './monitor-plan-comment.repository';
 
