c711b4f1861d4a9239918c370630be24d48b9b30
Jeffrey Aggrey <jeffreyaggrey@gmailcom>
2022-05-12 09:53:49 -0500

    export mp analyzer range

--- src/analyzer-range-workspace/analyzer-range.repository.ts
+++ src/analyzer-range-workspace/analyzer-range.repository.ts
@@ -5,4 +5,13 @@ import { AnalyzerRange } from '../entities/workspace/analyzer-range.entity';
 @EntityRepository(AnalyzerRange)
 export class AnalyzerRangeWorkspaceRepository extends Repository<
   AnalyzerRange
-> {}
+> {
+  async getAnalyzerRangesByCompIds(
+    componentIds: string[],
+  ): Promise<AnalyzerRange[]> {
+    return this.createQueryBuilder('ar')
+      .innerJoinAndSelect('ar.component', 'c')
+      .where('c.id IN (:...componentIds)', { componentIds })
+      .getMany();
+  }
+}
