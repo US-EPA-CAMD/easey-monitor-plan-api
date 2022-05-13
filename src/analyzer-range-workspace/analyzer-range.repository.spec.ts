1521766d8744c40fd62e770391baeae7409e73a5
subrataroy1 <subrataroy@cvpcorp.com>
2022-05-13 09:51:19 -0400

    test: add unit test for analyzer range

--- src/analyzer-range-workspace/analyzer-range.repository.spec.ts
+++ src/analyzer-range-workspace/analyzer-range.repository.spec.ts
@@ -3,6 +3,7 @@ import { SelectQueryBuilder } from 'typeorm';
 
 import { AnalyzerRangeWorkspaceRepository } from './analyzer-range.repository';
 import { AnalyzerRange } from '../entities/workspace/analyzer-range.entity';
+import { AnalyzerRangeBaseDTO } from '../dtos/analyzer-range.dto';
 
 const mockQueryBuilder = () => ({
   where: jest.fn(),
@@ -22,9 +23,7 @@ describe('AnalyzerRangeWorkspaceRepository', () => {
       ],
     }).compile();
 
-    analyzerRangeRepository = module.get(
-      AnalyzerRangeWorkspaceRepository,
-    );
+    analyzerRangeRepository = module.get(AnalyzerRangeWorkspaceRepository);
     queryBuilder = module.get<SelectQueryBuilder<AnalyzerRange>>(
       SelectQueryBuilder,
     );
@@ -37,16 +36,15 @@ describe('AnalyzerRangeWorkspaceRepository', () => {
         .mockReturnValue(queryBuilder);
       queryBuilder.where.mockReturnValue(queryBuilder);
       queryBuilder.andWhere.mockReturnValue(queryBuilder);
-      queryBuilder.getOne.mockReturnValue('mockAnalyzerRange');
+      queryBuilder.getOne.mockReturnValue(new AnalyzerRange());
 
       const result = await analyzerRangeRepository.getAnalyzerRangeByComponentIdAndDate(
-        0,
-        0
+        'componentId',
+        new AnalyzerRangeBaseDTO(),
       );
 
       expect(queryBuilder.getOne).toHaveBeenCalled();
-      expect(result).toEqual('mockAnalyzerRange');
+      expect(result).toEqual(new AnalyzerRange());
     });
   });
-
 });
--- src/analyzer-range-workspace/analyzer-range.service.spec.ts
+++ src/analyzer-range-workspace/analyzer-range.service.spec.ts
@@ -6,51 +6,21 @@ import { AnalyzerRangeMap } from '../maps/analyzer-range.map';
 import { AnalyzerRangeWorkspaceService } from './analyzer-range.service';
 import { AnalyzerRangeWorkspaceRepository } from './analyzer-range.repository';
 import { MonitorPlanWorkspaceService } from '../monitor-plan-workspace/monitor-plan.service';
-import { Repository } from 'typeorm';
-import { AnalyzerRange } from 'src/entities/workspace/analyzer-range.entity';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { AnalyzerRangeBaseDTO } from 'src/dtos/analyzer-range.dto';
+import {
+  AnalyzerRangeBaseDTO,
+  AnalyzerRangeDTO,
+} from '../dtos/analyzer-range.dto';
 
 jest.mock('../monitor-plan-workspace/monitor-plan.service.ts');
-
-const repositoryMockFactory = () => ({
-  find: jest.fn().mockResolvedValue(''),
-  findOne: jest.fn(entity => entity),
-});
-
-/* const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
-  getAnalyzerRangeByComponentIdAndDate: jest.fn(entity => entity),
-  // ...
-})); */
 
 const mockMap = () => ({
   one: jest.fn().mockResolvedValue(''),
   many: jest.fn().mockResolvedValue(''),
 });
 
-const analyzerRange = {
-  "id": "01968-GJ09-109A3C82EFD4412196C46611B6644257",
-  "componentRecordId": "01968-GJ09-892A2A2756B94347A4CFE444B7CD3DD5",
-  "analyzerRangeCode": "H",
-  "dualRangeIndicator": 0,
-  "beginDate": "2016-07-01",
-  "endDate": null,
-  "beginHour": 0,
-  "endHour": null,
-  "userId": "kezell",
-  "addDate": "2016-09-14",
-  "updateDate": "2016-10-30",
-  "active": true
-}
-
-const analyzerRangeImport = {
-  "analyzerRangeCode": "H",
-  "dualRangeIndicator": 0,
-  "beginDate": "2016-07-01",
-  "endDate": null,
-  "beginHour": 0,
-  "endHour": null
-}
+const mockRepo = () => ({
+  getAnalyzerRangeByComponentIdAndDate: jest.fn(),
+});
 
 describe('AnalyzerRangeWorkspaceService', () => {
   let service: AnalyzerRangeWorkspaceService;
@@ -64,7 +34,7 @@ describe('AnalyzerRangeWorkspaceService', () => {
         MonitorPlanWorkspaceService,
         {
           provide: AnalyzerRangeWorkspaceRepository,
-          useFactory: repositoryMockFactory,
+          useFactory: mockRepo,
         },
         {
           provide: AnalyzerRangeMap,
@@ -73,30 +43,52 @@ describe('AnalyzerRangeWorkspaceService', () => {
       ],
     }).compile();
 
-    service = module.get(AnalyzerRangeWorkspaceService);
-    repositoryMock = module.get(getRepositoryToken(AnalyzerRange));
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-
-  describe('getAnalyzerRanges', () => {
-    it('should return array of analyzer ranges', async () => {
-      const result = await service.getAnalyzerRanges(null);
-      expect(result).toEqual('');
-    });
+    service = module.get<AnalyzerRangeWorkspaceService>(
+      AnalyzerRangeWorkspaceService,
+    );
+    repositoryMock = module.get<AnalyzerRangeWorkspaceRepository>(
+      AnalyzerRangeWorkspaceRepository,
+    );
   });
 
   describe('importAnalyzerRange', () => {
-    it('should create analyzer range else updates', async () => {
-      repositoryMock.getAnalyzerRangeByComponentIdAndDate.mockReturnValue(analyzerRange);
-      expect(service.importAnalyzerRange(
+    const analyzerRangeImport = [new AnalyzerRangeBaseDTO()];
+    it('should create analyzer range if not exists', async () => {
+      repositoryMock.getAnalyzerRangeByComponentIdAndDate = jest
+        .fn()
+        .mockResolvedValue(null);
+      const createAnalyzerRange = jest
+        .spyOn(service, 'createAnalyzerRange')
+        .mockResolvedValue(new AnalyzerRangeDTO());
+      await service.importAnalyzerRange(
         'componentId',
         'locationId',
         analyzerRangeImport,
-        'userId')).toEqual(null);
-     expect(repositoryMock.getAnalyzerRangeByComponentIdAndDate).toHaveBeenCalledWith('componentId',analyzerRange);
+        'userId',
+      );
+      expect(
+        repositoryMock.getAnalyzerRangeByComponentIdAndDate,
+      ).toHaveBeenCalledWith('componentId', analyzerRangeImport[0]);
+      expect(createAnalyzerRange).toHaveBeenCalled;
+    });
+
+    it('should update analyzer range if exists', async () => {
+      repositoryMock.getAnalyzerRangeByComponentIdAndDate = jest
+        .fn()
+        .mockResolvedValue(new AnalyzerRangeDTO());
+      const updateAnalyzerRange = jest
+        .spyOn(service, 'updateAnalyzerRange')
+        .mockResolvedValue(new AnalyzerRangeDTO());
+      await service.importAnalyzerRange(
+        'componentId',
+        'locationId',
+        analyzerRangeImport,
+        'userId',
+      );
+      expect(
+        repositoryMock.getAnalyzerRangeByComponentIdAndDate,
+      ).toHaveBeenCalledWith('componentId', analyzerRangeImport[0]);
+      expect(updateAnalyzerRange).toHaveBeenCalled;
     });
   });
 });
