49edaee69e807d94a4c78442aecea73b8646c2a2
Jason Whitehead <jwhitehead@austin.rr.com>
2023-02-14 21:50:27 -0600

    fixing unit test

--- src/check-out/check-out.controller.spec.ts
+++ src/check-out/check-out.controller.spec.ts
@@ -5,7 +5,6 @@ import { LoggerModule } from '@us-epa-camd/easey-common/logger';
 import { MonitorPlanWorkspaceService } from '../monitor-plan-workspace/monitor-plan.service';
 import { UserCheckOutService } from '../user-check-out/user-check-out.service';
 import { CheckOutController } from './check-out.controller';
-import { CheckOutService } from './check-out.service';
 
 describe('CheckOutController', () => {
   let controller: CheckOutController;
@@ -16,7 +15,6 @@ describe('CheckOutController', () => {
       controllers: [CheckOutController],
       providers: [
         ConfigService,
-        CheckOutService,
         {
           provide: UserCheckOutService,
           useFactory: () => ({}),
