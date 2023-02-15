9c5cca0c5b9fb8457024c0a69edcec249b7939e6
Jason Whitehead <jwhitehead@austin.rr.com>
2023-02-14 21:37:58 -0600

    added websockets for checkouts and upgrade swagger-ui

--- src/check-out/check-out.module.ts
+++ src/check-out/check-out.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { UserCheckOutModule } from '../user-check-out/user-check-out.module';
-import { CheckOutService } from './check-out.service';
 import { CheckOutController } from './check-out.controller';
 import { MonitorPlanWorkspaceModule } from '../monitor-plan-workspace/monitor-plan.module';
 
 @Module({
-  imports: [UserCheckOutModule, MonitorPlanWorkspaceModule, HttpModule],
+  imports: [
+    UserCheckOutModule,
+    MonitorPlanWorkspaceModule,
+    HttpModule
+  ],
   controllers: [CheckOutController],
-  providers: [CheckOutService],
-  exports: [CheckOutService],
+  providers: [],
+  exports: [],
 })
 export class CheckOutModule {}
