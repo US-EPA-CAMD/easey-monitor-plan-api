f9d196a88a4f8141aeee03a103870484101e7419
Bill Miller <billmiller@cvpcorp.com>
2023-02-22 12:23:14 -0500

    Improving Unit Test Coverage

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -5,12 +5,7 @@ import { MessagesGateway } from './messages.gateway';
 import { UserCheckOutModule } from '../user-check-out/user-check-out.module';
 
 @Module({
-  imports: [
-    HttpModule,
-    UserCheckOutModule,
-  ],
-  providers: [
-    MessagesGateway,
-  ]
+  imports: [HttpModule, UserCheckOutModule],
+  providers: [MessagesGateway],
 })
 export class MessagesModule {}
