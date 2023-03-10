775323603da32b4ba4649bdd5a8b8af7acc2c4c6
Jordan Fontaine <jordanfontaine@cvpcorp.com>
2023-03-10 16:42:33 -0500

    SPAN-58-A fix

--- src/monitor-span-workspace/monitor-span-checks.service.ts
+++ src/monitor-span-workspace/monitor-span-checks.service.ts
@@ -135,7 +135,7 @@ export class MonitorSpanChecksService {
       // If ScaleTransitionPoint is not null
       if (monitorSpan.scaleTransitionPoint) {
         // If SpanValue is null and DefaultHighRangeValue is not null, return A
-        if (!monitorSpan.spanValue && monitorSpan.defaultHighRange) {
+        if (!monitorSpan.flowSpanValue && monitorSpan.defaultHighRange) {
           return this.getMessage('SPAN-58-A', {
             key: KEY,
           });
