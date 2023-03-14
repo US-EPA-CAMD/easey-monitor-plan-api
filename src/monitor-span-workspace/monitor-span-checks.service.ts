423b676f8e287f586e62e0bfb0b9ea074a83fa82
Jordan Fontaine <jordanfontaine@cvpcorp.com>
2023-03-14 11:38:13 -0400

    adding condtion param

--- src/monitor-span-workspace/monitor-span-checks.service.ts
+++ src/monitor-span-workspace/monitor-span-checks.service.ts
@@ -176,6 +176,7 @@ export class MonitorSpanChecksService {
   ): string {
     let error = null;
     let FIELDNAME: string = 'spanScaleTransitionPoint';
+    let CONDITION: string = 'Monitor Span Record'
     let scaleTransitionPoint = monitorSpan.scaleTransitionPoint;
 
     // For a Monitoring Span record with a valid ComponentTypeCode equal to "HG" or "HCL"
@@ -184,7 +185,7 @@ export class MonitorSpanChecksService {
       if (scaleTransitionPoint) {
         return this.getMessage('SPAN-61-A', {
           fieldname: FIELDNAME,
-          key: KEY,
+          conditon: CONDITION,
         });
       }
     }
