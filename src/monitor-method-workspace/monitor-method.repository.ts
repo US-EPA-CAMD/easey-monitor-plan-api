ab54c26729fe39b57683de90ec6d9e5d93f0154a
Bill Miller <billmiller@cvpcorp.com>
2023-05-02 14:40:13 -0400

    Update monitor-method.repository.ts

--- src/monitor-method-workspace/monitor-method.repository.ts
+++ src/monitor-method-workspace/monitor-method.repository.ts
@@ -10,17 +10,20 @@ export class MonitorMethodWorkspaceRepository extends Repository<
     locationId: string,
     parameterCode: string,
     beginDate: Date,
+    beginHour: number,
     endDate: Date,
-
+    endHour: number,
   ): Promise<MonitorMethod> {
     const result = this.createQueryBuilder('mme')
       .where('mme.locationId = :locationId', {
         locationId,
       })
+      .andWhere('mme.parameterCode = :parameterCode', {
+        parameterCode
+      })
       .andWhere(
-        '((mm.beginDate = :beginDate AND mm.beginHour = :beginHour) OR ((mm.endDate is not null) AND ( mm.endDate = :endDate AND mm.endHour = :endHour )))',
+        '(mm.beginDate = :beginDate AND mm.beginHour = :beginHour) OR (mm.endDate is not null AND mm.endDate = :endDate AND mm.endHour = :endHour)',
         {
-          parameterCode,
           beginDate,
           endDate,
         },
