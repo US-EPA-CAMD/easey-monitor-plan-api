530d95fb00864cb76473136f72f73457c70a64cc
Jordan Fontaine <jordanfontaine@cvpcorp.com>
2023-05-02 11:04:33 -0400

    updating MP Duct Waf

--- src/duct-waf-workspace/duct-waf.repository.ts
+++ src/duct-waf-workspace/duct-waf.repository.ts
@@ -7,13 +7,17 @@ export class DuctWafWorkspaceRepository extends Repository<DuctWaf> {
   async getDuctWafByLocIdBDateBHourWafValue(
     locationId: string,
     wafBeginDate: Date,
+    wafEndDate: Date,
     wafBeginHour: number,
+    wafEndHour: number,
     wafValue: number,
   ): Promise<DuctWaf> {
     return this.createQueryBuilder('dw')
       .where('dw.locationId = :locationId', { locationId })
       .andWhere('dw.wafBeginDate = :wafBeginDate', { wafBeginDate })
       .andWhere('dw.wafBeginHour = :wafBeginHour', { wafBeginHour })
+      .andWhere('dw.wafEndDate = :wafEndDate', { wafEndDate })
+      .andWhere('dw.wafEndHour = :wafEndHour', { wafEndHour })
       .andWhere('dw.wafValue = :wafValue', { wafValue })
       .getOne();
   }
