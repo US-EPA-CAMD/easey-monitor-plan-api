eeb7a27eb3e5303d5a00d0e3aead5aac2d82abf0
Jonas Hernandez <jonas.hernandez@erg.com>
2024-11-21 15:16:57 -0600

    updated system-component.service.spec.ts to properly run updateSystemComponent.

--- src/system-component-workspace/system-component.service.spec.ts
+++ src/system-component-workspace/system-component.service.spec.ts
@@ -22,6 +22,9 @@ const sysComps = [sysComp];
 
 const payload = new SystemComponentBaseDTO();
 
+const mockSystemComponent = new SystemComponent();
+mockSystemComponent.component = new Component();
+
 const repositoryFactory = () => ({
   getSystemComponents: jest.fn().mockResolvedValue(sysComps),
   getSystemComponent: jest.fn().mockResolvedValue(sysComp),
@@ -112,7 +115,7 @@ describe('SystemComponentWorkspaceService', () => {
     it('should update and return the system component', async () => {
       jest
         .spyOn(service, 'getSystemComponent')
-        .mockResolvedValue(new SystemComponent());
+        .mockResolvedValue(mockSystemComponent);
 
       const result = await service.updateSystemComponent({
         locationId: '1',
