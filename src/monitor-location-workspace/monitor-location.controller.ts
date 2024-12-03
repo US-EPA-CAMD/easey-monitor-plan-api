c673998907c7f85e574c2fdaf4c59024c4061fae
ergjustin <justin.joseph.erg.com>
2024-12-03 10:56:11 -0600

    Locations audit log implenent

--- src/monitor-location-workspace/monitor-location.controller.ts
+++ src/monitor-location-workspace/monitor-location.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ApiOkResponse, ApiSecurity, ApiTags } from '@nestjs/swagger';
-import { RoleGuard } from '@us-epa-camd/easey-common/decorators';
+import { AuditLog, RoleGuard } from '@us-epa-camd/easey-common/decorators';
 import { LookupType } from '@us-epa-camd/easey-common/enums';
 import { MonitorLocationDTO } from '../dtos/monitor-location.dto';
 import { MonitorLocationWorkspaceService } from './monitor-location.service';
@@ -26,6 +26,11 @@ export class MonitorLocationWorkspaceController {
     },
     LookupType.Location,
   )
+  @AuditLog({
+    label: 'Retrieved workspace location data',
+    requestParamsOutFields:['locId'],
+    responseBodyOutFields:['unitId', 'stackPipeId']
+  })
   getLocation(@Param('locId') locationId: string): Promise<MonitorLocationDTO> {
     return this.service.getLocation(locationId);
   }
@@ -39,6 +44,10 @@ export class MonitorLocationWorkspaceController {
     },
     LookupType.Location,
   )
+  @AuditLog({
+    label: 'Retrieved workspace relationship data',
+    requestParamsOutFields:['locId']
+  })
   async getLocationRelationships(@Param('locId') locId: string) {
     return this.service.getLocationRelationships(locId);
   }
