d2958deb8aaf98a69a990bc4cf9d5ce770ba04f3
ergjustin <justin.joseph.erg.com>
2024-12-04 17:28:58 -0600

    non workspace Spans audit log

--- src/monitor-span/monitor-span.controller.ts
+++ src/monitor-span/monitor-span.controller.ts
@@ -1,5 +1,6 @@
 import { ApiTags, ApiOkResponse, ApiSecurity } from '@nestjs/swagger';
 import { Get, Param, Controller } from '@nestjs/common';
+import { AuditLog } from '@us-epa-camd/easey-common/decorators';
 
 import { MonitorSpanDTO } from '../dtos/monitor-span.dto';
 import { MonitorSpanService } from './monitor-span.service';
@@ -15,6 +16,10 @@ export class MonitorSpanController {
     isArray: true,
     type: MonitorSpanDTO,
     description: 'Retrieves official span records for a monitor location',
+  })
+  @AuditLog({
+    label: 'Retrieved monitor location spans',
+    requestParamsOutFields: ['locId'],
   })
   getSpans(@Param('locId') locationId: string): Promise<MonitorSpanDTO[]> {
     return this.service.getSpans(locationId);
