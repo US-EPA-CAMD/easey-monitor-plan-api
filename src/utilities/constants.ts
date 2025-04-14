81e824235d56927e007e7b5c8ba537ce25b99abe
spetros-do <153679433+spetros-do@users.noreply.github.com>
2025-04-14 13:04:06 -0700

    Updated code to calculate dynamically to avoid static date issues, while allowing MP begin dates up to 90 days in future

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,8 +1,8 @@
 import moment from 'moment';
 
-export const MAXIMUM_FUTURE_DATE = moment()
-  .add(90, 'days')
-  .format('YYYY-MM-DD');
+export const getMaximumFutureDate = (): string =>
+  moment().add(90, 'days').format('YYYY-MM-DD');
+export const MAXIMUM_FUTURE_DATE = getMaximumFutureDate();
 export const MINIMUM_DATE = '1993-01-01';
 export const MIN_HOUR = 0;
 export const MAX_HOUR = 23;
